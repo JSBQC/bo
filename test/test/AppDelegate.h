05995a9b3cc7ae61ec705d76fccd9c25d8660aff
Bob Chen <sobluewhat@163.com>
2016-05-01 15:41:36 +0800

    上传

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 陈庆波 on 16/5/1.
+//  Copyright © 2016年 FreeBired. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 陈庆波 on 16/5/1.
+//  Copyright © 2016年 FreeBired. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
